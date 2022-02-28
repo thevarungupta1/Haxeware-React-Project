5dfd6e0c10d6ac430e1273dea1e43d30dfc4e540
Varun Gupta <thevarungupt1@gmail.com>
2022-02-28 18:58:25 +0530

    redux store implemented

---
+++ src/actions/index.js
@@ -0,0 +1,57 @@
+export const ADD_CART = 'ADD_CART';
+export const UPDATE_CART = 'UPDATE_CART';
+export const DELETE_CART = 'DELETE_CART';
+export const INCREASE_QUANTITY = 'INCREASE_QUANTITY'
+export const DECREASE_QUANTITY = 'DECREASE_QUANTITY'
+export const GET_ALL_PRODUCT = 'GET_ALL_PRODUCT'
+export const GET_NUMBER_CART = 'GET_NUMBER_CART'
+
+export function GetAllProduct(payload){
+    return{
+        type: 'GET_ALL_PRODUCT',
+        payload
+    }
+}
+
+export function GetNumberCart(){
+    return{
+        type: 'GET_NUMBER_CART'
+    }
+}
+
+export function AddToCart(payload){
+    return{
+        type: 'ADD_CART',
+        payload
+    }
+}
+
+export function UpdateCart(payload){
+    return{
+        type: 'UPDATE_CART',
+        payload
+    }
+}
+
+export function DeleteCart(payload){
+    return{
+        type: 'DELETE_CART',
+        payload
+    }
+}
+
+export function IncreaseQuantity(payload){
+    return{
+        type: 'INCREASE_QUANTITY',
+        payload
+    }
+}
+
+export function DecreaseQuantity(payload){
+    return{
+        type: 'DECREASE_QUANTITY',
+        payload
+    }
+}
+
+
---
+++ src/reducers/index.js
@@ -0,0 +1,74 @@
+import { combineReducers } from "redux";
+import {
+  GET_ALL_PRODUCT,
+  GET_NUMBER_CART,
+  ADD_CART,
+  DECREASE_QUANTITY,
+  INCREASE_QUANTITY,
+  DELETE_CART,
+} from "../actions/index";
+
+const initialState = {
+  numberCart: 0,
+  carts: [],
+  products: [],
+};
+
+function todoProduct(state = initialState, action) {
+  switch (action.type) {
+    case GET_ALL_PRODUCT:
+      return {
+        ...state,
+        products: action.payload,
+      };
+    case GET_NUMBER_CART:
+      return {
+        ...state,
+      };
+    case ADD_CART:
+      if (state.numberCart == 0) {
+        let cart = {
+          id: action.payload.id,
+          quantity: 1,
+          name: action.payload.name,
+          image: action.payload.image,
+          price: action.payload.price,
+        };
+        state.carts.push(cart);
+      } else {
+        let check = false;
+        state.carts.map((item, key) => {
+          if (item.id == action.payload.id) {
+            state.carts[key].quantity++;
+            check = true;
+          }
+        });
+        if (!check) {
+          let _cart = {
+            id: action.payload.id,
+            quantity: 1,
+            name: action.payload.name,
+            image: action.payload.image,
+            price: action.payload.price,
+          };
+          state.carts.push(_cart);
+        }
+      }
+      return {
+        ...state,
+        numberCart: state.numberCart + 1,
+      };
+    case INCREASE_QUANTITY:
+      state.numberCart++;
+      state.carts[action.payload].quantity++;
+      return {
+        ...state,
+      };
+  }
+}
+
+const ShopApp = combineReducers({
+  _todoProduct: todoProduct,
+});
+
+export default ShopApp;
